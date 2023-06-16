896521e8d0e6c16e30d381b0035823d8bc780096
Guilherme Pedrazzi <gsppedra@hotmail.com>
2023-06-16 15:58:41 -0300

    Adding axios and starting to work on front-end

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ApiComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ApiComponent from './components/ApiComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ApiComponent
   }
 }
 </script>
@@ -19,7 +20,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/ApiComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Exemplo de Consumo de API (EletroDomésticos)</h1>
+        <ul>
+            <li v-for="device in devices" :key="device.id">{{ device.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            devices: Object,
+        };
+    },
+    mounted() {
+        axios.get('http://127.0.0.1:8000/api/device')
+        .then(response => {
+            console.log(response.data.devices);
+            this.devices = response.data.devices;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    },
+};
+</script>
